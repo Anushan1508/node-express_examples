75cec99ca5b0d3fac9b4857e6d751ac50ece6a3b
Anushan Santhirakumar <anushan1508@gmail.com>
2021-03-22 02:59:41 +0530

    node modules

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
-const http = require('http')
+const _ = require('lodash')
+const items = [1, [3,4,[8,9,[2]]]];
 
-const server = http.createServer((req, res) => {
-    if(req.url === '/') {
-        res.end('Welcome to home page')
-    }
-    if(req.url === '/about') {
-        res.end('Here is our history')
-    }
-    res.end(`
-        <h1>Oops!</h1>
-        <p> We can't seem to find thr pagr you are looking for </p>
-        <a href  ="/"> Back to home</a> 
-    `)
-
-})
-
-server.listen(5000)
+const newItems = _.flattenDeep(items)
+console.log(items)
+console.log(newItems)
