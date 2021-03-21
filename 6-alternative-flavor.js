6694e00324c42cfefd8ac208e77a5c3334275679
Anushan Santhirakumar <anushan1508@gmail.com>
2021-03-21 20:01:29 +0530

    added

---
+++ 6-alternative-flavor.js
@@ -0,0 +1,8 @@
+module.exports.items = ['item1', 'item2']
+
+const member = {
+    name: 'Anu',
+    age: 23
+}
+
+module.exports.singleMember = member
--- app.js
+++ app.js
@@ -2,7 +2,10 @@
 // Modules - Encapsulated code ((Only share minimum))
  const names = require('./4-names')
  const sayHi = require('./5-utils')
+ const data = require('./6-alternative-flavor')
+ require('./7-mind-grenade')
 
-sayHi(names.vithu);
-sayHi(names.tharuni);
-sayHi('Anushan');
+
+// sayHi(names.vithu);
+// sayHi(names.tharuni);
+// sayHi('Anushan');
