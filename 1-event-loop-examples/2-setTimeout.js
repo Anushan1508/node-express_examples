fe23bc46f240244c450327d90f2fd4cd797f526d
Anushan Santhirakumar <anushan1508@gmail.com>
2021-03-22 14:36:28 +0530

    event

---
+++ 1-event-loop-examples/2-setTimeout.js
@@ -0,0 +1,7 @@
+// Staring Operating Systme
+console.log('first')
+setTimeout(() => {
+    console.log('second')
+}, 0)
+console.log('third')
+//End OS
---
+++ 1-event-loop-examples/3-setIntervel.js
@@ -0,0 +1,7 @@
+setInterval(() => {
+    console.log('Hello Anushan')
+}, 2000)
+console.log('I will run first')
+// Process stays alive unless
+// Kill process CONTROL +C
+// Unexpected errror
